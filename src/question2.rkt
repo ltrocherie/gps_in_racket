85d06ef867188612ada256bfa431833a4be63c80
Ouedraogo Sonia De Laeticia <Sonia_De_Laeticia.Ouedraogo@bordeaux-inp.fr>
2019-03-25 19:29:42 +0100

    ajout de l'algo de suppression des noeuds de degre 2

--- src/question2.rkt
+++ src/question2.rkt
@@ -1,102 +1,74 @@
 #lang racket
 (require xml)
 
-;(xml->xexpr (document-element
- ;   (read-xml (open-input-file "../maps/projMapping.osm"))))
 
-;;(define (simplify l)
-  ;;(cadr l))
+;;Precond: n1 et n2 noeuds voisins et n1 de degre 2
+;;Postcond: n2 avec voisin modifiés
+;;Action: enleve n1 comme voisin de n2  et lui ajoute à n2 l'autre voisin de n1
+;;exemple (manage_node '(5 (0.1 6) (1 4)) '(1 (2 8) (9 5 6))) --> '(1 (2 8) (9 4 6)))
+(define (manage_node n1 n2) 
+  (append (list (car n2) (cadr n2))
+          (list (cons (cadr (caddr n1)) (remove (car n1) (caddr n2))))))
 
-(define (del c1 c2) ;;concatene deux noeuds en supprimant l'element commun
-   (cond
-     [(= (car c1) (car c2)) (list (cadr c1) (cadr c2))]
-     [(= (car c1) (cadr c2)) (list (cadr c1) (car c2))]
-     [(= (cadr c1) (car c2)) (list (car c1) (cadr c2))]
-     [else (list (car c1) (car c2))]))
 
-;;(del '(13 12) '(12 14))
-
-(define (all_nodes l) ;;concatene les couples dans une liste en gardant les occurences
-  (if (null? l)
-      '()
-      (append (car l) (all_nodes (cdr l)))))
-
-;;(all_nodes '((4 7) (4 6) (1 5)))
-
-(define (insert_int x l) ;;insert un element dans une liste en gardant l'ordre
-  (cond
-    [(null? l) (cons x l)]
-    [(>= (car l) x) (cons x l)]
-    [(<= (car l) x) (cons (car l) (insert_int x (cdr l)))]))
-
-(define (sort_numbers l) ;; trie une liste dans l'ordre croissant
-  (if (null? l)
+;;Precond: node un noeud de degre 2 et l une liste de noeuds
+;;Postcond: l une liste de neouds
+;;Action: applique manage_node au premier voisin de node qui serait dans l
+;;exemple (concat_neighbour '(5 (0.1 6) (1 4)) '((5 (0.1 6) (1 4)) (1 (2 8) (9 5 6)) (4 (0 7) (5 3)))) --> '((5 (0.1 6) (1 4)) (1 (2 8) (9 4 6)) (4 (0 7) (5 3))))
+(define (concat_neighbour node l)
+  (if (equal? (assoc (caaddr node) l) #f)
       l
-      (insert_int (car l) (sort_numbers (cdr l)))))
-
-;;(sort_numbers (all_nodes '((4 7) (4 6) (1 5))))
-
-;;(define (useless_nodes sorted_nodes useless)
-  ;;useless = listes des noeud apparaissant exactement 2 fois
+      (cons (manage_node node (assoc (caaddr node) l))
+            (remove (assoc (caaddr node) l) l))))
 
 
-;;(define (search n l )
-  ;;(cond
-    ;;[(=
-
-(define (delete node l)
-  (remove node l))
-
-(define (manage_node n1 n2)  ;;ex (manage_node '(5 (1 4)) '(4 (9 5 6))) retourne '(4 (1 9 6)
-  (append (list (car n2))
-          (list (cons (cadadr n1) (remove (car n1) (cadr n2))))))
-
-;(manage_node '(5 (1 4)) '(4 (9 5 6)))
-
-(define (concat_neighbour node l) 
-  (cons (manage_node node (assoc (caadr node) l))
-        (remove (assoc (caadr node) l) l)))
-
+;;Precond: node un noeud de degre 2 et l une liste de noeuds
+;;Postcond: l une liste de neouds
+;;Action: applique manage_node aux deux voisins de node qui seraient dans l
+;;exemple: (concat_neighbourS '(5 (0.1 6) (1 4)) '((5 (0.1 6) (1 4)) (1 (2 8) (9 5 6)) (4 (0 7) (5 3)))) --> '((5 (0.1 6) (1 4)) (1 (2 8) (9 4 6)) (4 (0 7) (1 3))))
 (define (concat_neighbourS node l)
-  (concat_neighbour (cons (car node) (list (reverse (cadr node))))
+  (concat_neighbour (append (list (car node) (cadr node)) (list (reverse (caddr node))))
                     (concat_neighbour node l)))
 
+
+;;Precond: node un noeud
+;;Postcond: un bouleen
+;;Action: retourne #t si node est de degre2 et #f sinon
+;;exemple: (is_deg2 '(5 (0.1 6) (1 4))) --> #t et (is_deg2 '(5 (0.1 6) (1 4))) --> #f
 (define (is_deg2? node)
-  (if (= (length (cadr node)) 2)
+  (if (= (length (caddr node)) 2)
       #t
       #f))
-(concat_neighbourS '(2 (1 3)) '((1 (2)) (3 (2 4 7)) (4 (3 5 6)) (5 (4 6)) (6 (5 7)) (7 (6))))
-(concat_neighbourS '(5 (4 6)) '((1 (2)) (3 (2 4 7)) (4 (3 5 6)) (5 (4 6)) (6 (5 7)) (7 (6))))
-(concat_neighbourS '(6 (5 7)) '((1 (2)) (3 (2 4 7)) (4 (3 5 6)) (5 (4 6)) (6 (5 7)) (7 (6))))
 
-;(is_deg2? '(5 (7 8)))
 
-(define (nodes_deg2 l) ;;retourne la liste des noeuds de degré 2 extraits de l
+;;Precons: une liste de noeuds
+;;Postcond: une liste de noeuds
+;;Action: retourne la liste de tous les noeuds de degree 2 contenues dans la liste en entree
+;;exemple: (nodes_deg2 '((5 (0.1 6) (1 4)) (1 (2 8) (9 5 6)) (4 (0 7) (5 3)))) --> '((5 (0.1 6) (1 4)) (4 (0 7) (5 3))))
+(define (nodes_deg2 l)
   (if (null? l)
       '()
       (if (is_deg2? (car l))
           (cons (car l) (nodes_deg2 (cdr l)))
           (nodes_deg2 (cdr l)))))
 
-;(nodes_deg2 '((1 (2)) (2 (1 3)) (3 (2 4 7)) (4 (3 5 6)) (5 (4 6)) (6 (5 7)) (7 (6))))
 
+;;Precond: liste nodes_deg2_liste, deux listes de noeuds "nodes_deg2_liste" etant la liste des noeuds de degre 2 de "liste"
+;;Postcond: une liste de noeuds
+;;Action: enleve de "liste" tous les noeuds de degree 2 en ayant de soin de bien traiter d'abord les voisins de ceux-ci
+;;exemple: (remove_nodes_deg2 '((1 (1 5) (2)) (3 (0.7 4) (2 4 7)) (4 (11 0.9) (3 5 9)) (7 (6 4) (6 3)) (5 (1 8.0) (4 6)) (6 (12 45) (5 7)) (2 (3 0) (1 3)))
+;;                            '((1 (1 5) (2)) (7 (6 4) (6 3)) (5 (1 8.0) (4 6)) (6 (12 45) (5 7)) (2 (3 0) (1 3))) --> '((3 (0.7 4) (1 6 4)) (1 (1 5) (3)) (4 (11 0.9) (6 3 9)))
 (define (remove_nodes_deg2 liste nodes_deg2_liste)
   (if (null? nodes_deg2_liste)
       liste
-      (remove_nodes_deg2 (concat_neighbourS (car nodes_deg2_liste) liste) (cdr nodes_deg2_liste))))
+      (remove_nodes_deg2 (concat_neighbourS (car nodes_deg2_liste) (remove (assoc (caar nodes_deg2_liste) liste) liste))
+                         (cdr nodes_deg2_liste))))
 
+
+;;Precond: l une liste de noeuds
+;;Postcond: une liste de noeuds
+;;Action: supprime d'une liste fournie tous des noeuds de degre 2 sans qu'on fournisse la liste de ces noeuds en parametre
+;;exemple: (remove_nodes_deg2 '((1 (1 5) (2)) (3 (0.7 4) (2 4 7)) (4 (11 0.9) (3 5 9)) (7 (6 4) (6 3)) (5 (1 8.0) (4 6)) (6 (12 45) (5 7)) (2 (3 0) (1 3))))
+                        ;;--> '((3 (0.7 4) (1 6 4)) (1 (1 5) (3)) (4 (11 0.9) (6 3 9)))
 (define (graph_without_deg2_nodes l)
   (remove_nodes_deg2 l (nodes_deg2 l)))
-
-;(graph_without_deg2_nodes '((1 (2)) (2 (1 3)) (3 (2 4 7)) (4 (3 5 6)) (5 (4 6)) (6 (5 7)) (7 (6 0)))) A TERMINER
-         
-;;(delete '(6 (3 5)) '((4 (7 8)) (6 (3 5)) (1 (2 0))))
-;(cons (car '(6 (3 5))) (list (reverse (cadr '(6 (3 5))))))
-;(concat_neighbour '(6 (3 5)) '((3 (6 8)) (5 (1 6)) (1 (2 0))))
-;(concat_neighbourS '(6 (3 5)) '((3 (6 8)) (5 (1 6)) (1 (2 0))))
-;(caadr '(6 (3 5)))
-;(assoc (caadr '(6 (3 5))) '((3 (6 8)) (5 (3 6)) (1 (2 0))))
-
-;(nodes_deg2 '((3 (6 8 7)) (4 (6 8)) (7 (8 9 0)) (4 (0 9))))
-
-;(assoc 5 '( (5 (6 7 5) (3 6))))
