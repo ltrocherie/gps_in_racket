17c5a67cbb4a99fd615ef85e6f9908ccfd78a81f
Pavia Pierre <Pierre.Pavia@bordeaux-inp.fr>
2019-04-16 15:17:37 +0200

    adaptation aux dernières modifications des parties de marc et sonia

--- src/interprete.rkt
+++ src/interprete.rkt
@@ -6,7 +6,7 @@
 
 (require "conv_xml.rkt")
 (require "qu3.rkt")
-(require "svg.rkt")
+;(require "svg.rkt")
 (require xml)
 (require web-server/servlet)
 
@@ -17,7 +17,10 @@
 ;en attendant d'avoir la fonction qui renvoie du response/xexpr
 ;avec le chemin demandé
 (define (aff_graph argue)
-  #f)
+;let les coordonnées min et max du graphe général
+;afficher le graphe général en noir et le graphe spécifique en rouge avec les min et max du général
+  (create_html_responses "OPEN MAPPING SERVICE REAL DISPLAY PAGE" "800" "800" data argue (roam-bounds t) "black" "red")
+)
 
 ;cherche un chemin entre les points start et end
 ;si le chemin existe, elle renvoie un response/xexpr avec le code HTML SVG
@@ -26,32 +29,33 @@
 (define (route start end)
   ;(let ([onwei (one_way start end data)]);avec one_way la fonction qui trouve le chemin entre start et end à travers data
   (let ([findwei (find_way start end data)]);avec find_way la fonction qui trouve le chemin entre start et end à travers data
-  (if (null? findwei)
-      (response/output
-        #:mime-type TEXT/HTML-MIME-TYPE
-        (lambda (out)
-          (fprintf out "Disconnected Universe Error~n")
-          ));renvoie l'erreur demandée
-      (aff_graph (shapeshift (cdr findwei)))
-      )))
+    (if (null? findwei)
+        (response/output
+         #:mime-type TEXT/HTML-MIME-TYPE
+         (lambda (out)
+           (fprintf out "Disconnected Universe Error~n")
+           ));renvoie l'erreur demandée
+        ;(aff_graph (shapeshift (cdr findwei)))
+        (aff_graph (cdr findwei)) ;maintenant le cdr est sous la forme graph
+        )))
 
 ;cherche le chemin le plus court entre start et end
 ;renvoie sa distance en response/xexpr avec le code HTML SVG si il existe
 ;renvoie le message d'erreur sinon
 (define (distance start end)
   (let ([findwei (find_way start end data)]);avec find_way la fonction qui trouve le chemin entre start et end à travers data
-  (if (null? findwei)
-      (response/output
-        #:mime-type TEXT/HTML-MIME-TYPE
-        (lambda (out)
-          (fprintf out "Disconnected Universe Error~n")
-          ));renvoie l'erreur demandée
-      (response/output
-        #:mime-type TEXT/HTML-MIME-TYPE
-        (lambda (out)
-          (fprintf out (string-append "La distance demandée est : " (number->string (car findwei)) "."))
-          ));renvoie la distance demandée
-      )))
+    (if (null? findwei)
+        (response/output
+         #:mime-type TEXT/HTML-MIME-TYPE
+         (lambda (out)
+           (fprintf out "Disconnected Universe Error~n")
+           ));renvoie l'erreur demandée
+        (response/output
+         #:mime-type TEXT/HTML-MIME-TYPE
+         (lambda (out)
+           (fprintf out (string-append "La distance demandée est : " (number->string (car findwei)) "."))
+           ));renvoie la distance demandée
+        )))
 
 ;cherche le cycle le plus court reliant tous les points de la liste nodes
 ;affiche soit la représentation graphique de l'itinéraire circulaire
@@ -66,13 +70,11 @@
       (if (null? (cdr l))
           (cdr l)
           (cons (list (search-cord (car l) data) (list (search-cord (cadr l) data))) (shapeshift (cdr l)))
+          )
       )
   )
-)
 
 (define t (xml->xexpr (document-element
                        (read-xml (open-input-file (command-line #:args (filename) filename))))))
-;                       (read-xml (open-input-file "./maps/forrest-testloop.osm")))))
+                       ;(read-xml (open-input-file "./maps/forrest-testloop.osm")))))
 (define data (append-succs (roam-node t) (roam-way t)))
-
-;(define (distance start end))
